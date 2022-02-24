ee829b438041bcc4bc0440c34dc9261dc09442af
Peter Janku <janku@utb.cz>
2022-02-24 21:00:58 +0100

    Implement rest functionality of ImageBuffer. Necessary uint test were added. Improved documentation.
    Signed-off-by: Peter Janku <janku@utb.cz>

--- tests/ImageBuffer_test.cc
+++ tests/ImageBuffer_test.cc
@@ -12,6 +12,8 @@ TEST(ImageBufferTest, InitialConditions) {
     EXPECT_EQ(buffer.getFree(), 4);
 }
 
+
+
 TEST(ImageBufferTest, SimplePushFetch) {
     ImageBuffer<int, 4> buffer;
 
@@ -28,4 +30,49 @@ TEST(ImageBufferTest, SimplePushFetch) {
     buffer.pushOverride(6);
     EXPECT_EQ(buffer.getLastIndex(), 1);
     EXPECT_EQ(buffer.fetchLast(), 6);
+}
+
+TEST(ImageBufferTest, IndexIncrementing) {
+    ImageBuffer<int, 4> buffer;
+    EXPECT_EQ(buffer.getLastIndex(),-1);
+    EXPECT_EQ(buffer.getCurrentIndex(),-1);
+
+    buffer.pushOverride(1);
+    EXPECT_EQ(buffer.getLastIndex(),0);
+    EXPECT_EQ(buffer.getCurrentIndex(),0);
+    buffer.pushOverride(1);
+    EXPECT_EQ(buffer.getLastIndex(),1);
+    EXPECT_EQ(buffer.getCurrentIndex(),0);
+    buffer.pushOverride(1);
+    buffer.pushOverride(1);
+    buffer.pushOverride(1);
+    EXPECT_EQ(buffer.getLastIndex(),0);
+    EXPECT_EQ(buffer.getCurrentIndex(),1);
+    buffer.pushOverride(1);
+    buffer.pushOverride(1);
+    buffer.pushOverride(1);
+    EXPECT_EQ(buffer.getLastIndex(),3);
+    EXPECT_EQ(buffer.getCurrentIndex(),0);
+}
+
+TEST(ImageBufferTest, ComplexPushFetch){
+    ImageBuffer<int, 4> buffer;
+
+    bool tmp = true;
+    buffer.fetchCurrent(tmp);
+    EXPECT_EQ(tmp, false);
+    buffer.pushOverride(1);
+    buffer.pushOverride(2);
+    buffer.pushOverride(3);
+    buffer.pushOverride(4);
+    EXPECT_EQ(buffer.fetchCurrent(tmp),1);
+    EXPECT_EQ(tmp, true);
+    EXPECT_EQ(buffer.fetchCurrent(tmp),2);
+    EXPECT_EQ(tmp, true);
+    EXPECT_EQ(buffer.fetchCurrent(tmp),3);
+    EXPECT_EQ(tmp, true);
+    EXPECT_EQ(buffer.fetchCurrent(tmp),4);
+    EXPECT_EQ(tmp, true);
+    EXPECT_EQ(buffer.fetchCurrent(tmp),0);
+    EXPECT_EQ(tmp, false);
 }
--- tools/ImageBuffer.h
+++ tools/ImageBuffer.h
@@ -7,6 +7,7 @@
 #include <QMutex>
 #include <QMutexLocker>
 #include <QDebug>
+#include <QObject>
 /**
  * Template class for storing set of continuous data.
  * The data are stored in array - the class is working like a circular buffer. The data in buffer can be be overwrite
@@ -16,6 +17,7 @@
  */
 template<class T, int size = 2>
 class ImageBuffer {
+
 public:
     /**
      * Return the size of circular buffer
@@ -36,20 +38,48 @@ public:
     int getCurrentIndex();
 
     T fetchLast();
+    T fetchLast(bool &result);
 
-    bool push();
+    T fetchCurrent();
+    T fetchCurrent(bool &result);
+
+
+    bool push(T value);
+
 
     bool pushOverride(T value);
 
-
 protected:
-    T buffer[size];
-    QMutex manipulationMutex;
-    int last = -1;
-    int curent = -1;
+    int incrementLastPointer();
+    int incrementCurrentPointer();
+
+    T buffer[size];             /**< memory array for the buffer storage */
+    QMutex manipulationMutex;   /**< internal mutext */
+    int last = -1;              /**< index of last added item */
+    int current = -1;           /**< index of oldest item in buffer which was not fetched */
     int free = size;
 };
 
+/**
+ * This function will correctly increment the buffer pointers.
+ * @return
+ */
+template<class T, int size>
+int ImageBuffer<T, size>::incrementLastPointer() {
+    if (last == -1){
+        last = 0;
+        current = 0;
+    }else{
+        if ((++last) == size)  last = 0;
+        if (last == current) {
+            if ((++current) == size) current = 0 ;
+        }
+    }
+    return  last;
+}
+
+
+//TODO: reipmlement the "free" space counter - current implementation is not working
 /**
  * Return amount of free (non used) elements in buffer
  * @return Number of free elements
@@ -66,27 +96,104 @@ int ImageBuffer<T, size>::getLastIndex() {
 
 template<class T, int size>
 int ImageBuffer<T, size>::getCurrentIndex() {
-    return 0;
+    return current;
 }
 
+//TODO: Solve the error part of the program.
+/**
+ * Get the lastest added element from the buffer
+ * @param result the last added element
+ * @return true if success, false if no elements are in the buffer
+ */
+template<class T, int size>
+T ImageBuffer<T, size>::fetchLast(bool &result) {
+    QMutexLocker lock(&manipulationMutex);
+    if (this->getLastIndex() != -1){
+        result = true;
+        return buffer[this->getLastIndex()];
+    }
+    else{
+        result = false;
+        //return nullptr;
+    }
+}
+/**
+ * Get the lastest added element from the buffer
+ * @return true if success, false if no elements are in the buffer
+ */
 template<class T, int size>
 T ImageBuffer<T, size>::fetchLast() {
-    QMutexLocker lock(&manipulationMutex);
-    return buffer[this->getLastIndex()];
+    bool tmp;
+    return fetchLast(tmp);
 }
 
+/**
+ * Push new element at the top of the buffer and return true only if there is a space. If the buffer is full, element isn't pushed and return value is false.
+ * @param value new value to be pushed
+ * @return true if pushed, false if not
+ */
 template<class T, int size>
-bool ImageBuffer<T, size>::push() {
+bool ImageBuffer<T, size>::push(T value) {
+    QMutexLocker lock (&manipulationMutex);
+    if (getFree() > 0) {
+        incrementLastPointer();
+        buffer[last]  = value;
+        qDebug() << "New element in empty space...";
+        return true;
+    }
     return false;
 }
 
+
+/**
+ * Push the new value at the top of the buffer even if old unprocessed image is already there.
+ * @param value new value to be pushed
+ * @return alvays true (planned for further use)
+ */
 template<class T, int size>
 bool ImageBuffer<T, size>::pushOverride(T value) {
     QMutexLocker lock(&manipulationMutex);
-    if ((++last) == size)  last = 0;
+    incrementLastPointer();
     buffer[last]  = value;
-    qDebug() << "New element...";
-    return false;
+    qDebug() << "New element ...";
+    return true;
+}
+/**
+* Increment the current index. If there isn't unfetched element, both counters are reset to -1
+ * @return Original value before increment.
+ */
+template<class T, int size>
+int ImageBuffer<T, size>::incrementCurrentPointer() {
+    int tmp = current;
+    if (current > -1){
+        if ((++current) == size) current = 0;
+        if (tmp == last){
+            current = -1;
+            current = -1;
+        }
+    }
+    return tmp;
+}
+
+template<class T, int size>
+T ImageBuffer<T, size>::fetchCurrent() {
+    bool tmp;
+    return fetchCurrent(tmp);
+}
+
+//TODO: Solve the error part of the program.
+template<class T, int size>
+T ImageBuffer<T, size>::fetchCurrent(bool &result) {
+    QMutexLocker lock(&manipulationMutex);
+    int tmp = incrementCurrentPointer();
+    if (tmp != -1){
+        result = true;
+        return buffer[tmp];
+    }
+    else{
+        result = false;
+        //return nullptr;
+    }
 }
 
 
